204d7c1e5c8e1800b779b8010bb631c4d23cda72
fabienfleureau <fabien.fleureau@gmail.com>
2014-07-30 21:22:20 +0200

    Repository initialisation.

---
+++ xtextatstackoverflow/src/main/java/fr/fabienfleureau/xtextatstackoverflow/App.java
@@ -0,0 +1,11 @@
+package fr.fabienfleureau.xtextatstackoverflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
